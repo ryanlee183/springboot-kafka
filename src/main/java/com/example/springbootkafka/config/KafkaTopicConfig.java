3a8489ff2b5ca961696b4f47a99be9399c7a2962
ryanlee183 <ryanlee183@hotmail.com>
2024-03-28 11:21:49 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootkafka/config/KafkaTopicConfig.java
@@ -0,0 +1,20 @@
+package com.example.springbootkafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic myTopic() {
+        return TopicBuilder.name("topic1").build();
+    }
+
+    @Bean
+    public NewTopic myJsonTopic() {
+        return TopicBuilder.name("topic1_json").build();
+    }
+}
